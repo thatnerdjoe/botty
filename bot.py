3870bf9d8ad1238f8d92b7c054ad6be7a60b5b43
Joe M <jamill42@email.arizona.edu>
2021-03-19 21:59:59 -0700

    Added feature: offloaded token into config.json

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+# Import python modules
 import discord
 import logging
+import json
 from time import time
+
+# Import bascobot function modules
 
 
 def performance(fn):
@@ -44,20 +48,53 @@ def constant(fn):
 
 class _Const(object):
     '''
-    This class is merely a construct to hold constant values.
+    This class is merely a construct to enforce constant values.
     Each value is represented by a function holding only a return value.
-    To get the value, call it with (assuming CONST is an object of _Const): 
-    >>> CONST.SAMPLE 
+    To get the value, call it with (assuming CONST is an object of _Const):
+    >>> CONST.SAMPLE
     '''
     # Holds location of the bot's config file
     @constant
     def CONFIG():
-        return './config'
+        config_file = './config.json'
+        try:
+            data = json.load(open(config_file, 'r'))
+            return data
+        except Exception as e:
+            print(f'ERROR: could not open find config file {config_file}')
+            exit()
 
     # Sample constant, returns a silly value for testing
-    @constant
+    @ constant
     def SAMPLE():
         return 0xCABB005E
 
 
+# Instantiate client object
 client = discord.Client()
+
+# Instantiate CONSTANTS object
+CONST = _Const()
+
+# specify bot command prefixes using value in the config.json file
+bot_command = bot.command(command_prefix=CONST.CONFIG['prefix']
+
+
+@ client.event
+async def on_ready():
+    print('Logged on as {0.user}'.format(client))
+
+
+@ client.event
+async def on_message(msg):
+    # Do nothing if message is from this box
+    if msg.author == client.user:
+        return
+
+    # If a server message has the command prefix, do a task
+    if message.content.startswith(bot_command):
+        await message.channel.send()
+
+
+# Import the API token
+client.run(CONST.CONFIG['token'])
