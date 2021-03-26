11e5b2a277577a456c2e61bd4c517496e0aef7a4
HeroicosHM <houghtonawe@gmail.com>
2021-03-26 01:56:31 -0700

    Fixed minor multi-line comment syntax
    Signed-off-by: HeroicosHM <houghtonawe@gmail.com>

--- cogs/botty.py
+++ cogs/botty.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-'''
+"""
 *******************************************************************************
 This is a Cog. These structures are used by Discord.py to create classes
 with their own commands, event listeners, and attributes.
@@ -12,7 +12,7 @@ Authors: Joe Miller (@thatnerdjoe)
 Version: 0.1
 Date: 03-20-2021
 *******************************************************************************
-'''
+"""
 
 
 class Botty(commands.Cog):
